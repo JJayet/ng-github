35cd6c3bb8313ac3b8e4855953f818fa84c047be
Jonathan Jayet <jonathan@artus.io>
2017-11-18 12:36:13 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatCardModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule } from '@angular/material'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-
-import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component'
+import { FooterComponent } from './footer/footer.component'
+import { GithubService } from './services/github.service'
+import { BodyComponent } from './body/body.component'
+import { AppComponent } from './app.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { environment } from './environments/environment'
+import { enableProdMode } from '@angular/core'
+import { AppModule } from './app/app.module'
+import { hmrBootstrap } from './hmr'
+import 'hammerjs'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+if (environment.production) enableProdMode()
 
-if (environment.production) {
-  enableProdMode();
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule)
+
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap)
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!')
+    console.log('Are you using the --hmr flag for ng serve?')
+  }
+} else {
+  bootstrap()
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
